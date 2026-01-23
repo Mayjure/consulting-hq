5f30de08a258fbe5dc438812fc3f77c697cbd259
Mayjure <76072497+Mayjure@users.noreply.github.com>
2026-01-23 12:11:58 -0500

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,1 +1,11 @@
+/* =============================== */
+/* ADMIN CONFIGURATION */
+/* =============================== */
+
+const SETTINGS = {
+    referralDonation: 1000,
+    consultDonationHourly: 500,
+    testReferralLimit: 2,
+    batchSize: 4
+};
 
